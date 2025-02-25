db04a6d1f0aab07e88dfcb6ddfcb5a2bf020ed2c
Harrison Jansen van Beek <156035437+harrisonjansenva@users.noreply.github.com>
2025-02-25 15:44:01 -0700

    Update top.v

--- top.v
+++ top.v
@@ -3,22 +3,14 @@ module top(
     output [15:0] led
     
 );
-integer i,
-integer num_sw;
-
-initial begin
-    num_sw = 16;
-    led[0] = ~sw[0];
-end
-
-
-
-always @(*)
-begin
-    for (i = 1; i < num_sw; i = i + 1)
+    assign led[0] = ~sw[0];
+     
+ genvar i;
+ generate 
+    for (i = 1; i < 16; i = i + 1)
     begin
-        led[i] = led[i - 1] ^ sw[i];
+         assign led[i] = led[i - 1] ^ sw[i];
     end
-end
+endgenerate 
     
 endmodule
