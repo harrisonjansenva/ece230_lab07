472bc85da79c9de02c5a09d64c371940ed0306ee
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-02-25 13:36:01 -0700

    updated functonality for code

--- top.v
+++ top.v
@@ -1,7 +1,26 @@
 module top(
     input [15:0] sw,
     output [15:0] led
+    
 );
+integer i,
+integer num_sw;
+
+initial begin
+    num_sw = 16;
+    led[0] = ~sw[0];
+end
+
+
+
+always @(*)
+begin
+    for (i = 1; i < num_sw; i = i + 1)
+    begin
+        led[i] = led[i - 1] ^ sw[i];
+    end
+end
+
     
     
 endmodule
